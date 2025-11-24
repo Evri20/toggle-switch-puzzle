d38d19a3de7f371463e6067523954cc879b2b758
Evri20 <158607847+Evri20@users.noreply.github.com>
2025-11-24 12:16:54 -0500

    final

--- toggle switch puzzle/Assets/toggles/gameManager.cs
+++ toggle switch puzzle/Assets/toggles/gameManager.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class gameManager : MonoBehaviour
 {
+    public TextMeshProUGUI text;
     bool keyCode = false;
     public List<GameObject> lights;
     // Start is called before the first frame update
@@ -15,9 +16,38 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (lights = Color.green)
+        if (keyCode)
+        {
+            text.enabled = true;
+        }
+        else 
+        {
+            text.enabled = false;
+        }
+    }
+    public void checkWin()
+    {
+        int lightsOn = 0;
+         foreach (var items in lights)
+        {
+            
+            if (items.GetComponent<toggle_1>().ison)
+            {
+                lightsOn ++; 
+            }
+        }
+        if (lightsOn == 6)
         {
             keyCode = true;
+        }
+    }
+    public void Reset()
+    {
+        foreach (var items in lights)
+        {
+            items.GetComponent<toggle_1>().ison = false;
+            items.GetComponent<SpriteRenderer>().color = Color.red;
+            keyCode = false;
         }
     }
 }
